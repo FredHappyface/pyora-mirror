834adc921cdd524e42fee53a033c451b7e56a308
InkLab <inklabapp@gmail.com>
2020-06-15 17:33:47 -0400

    -added test case for single group layer ordering with add_layer

--- test/test_render.py
+++ test/test_render.py
@@ -2,6 +2,7 @@ from pyora import Project, Renderer, TYPE_LAYER
 from PIL import Image
 import os, sys
 from utils import getf
+import numpy as np
 
 def test_basic_render(tmp_path):
 
@@ -15,4 +16,36 @@ def test_basic_render(tmp_path):
     img = Image.open(save_to)
 
     assert img.size == (100, 117)
+
+def test_add_layer_ordering(tmp_path):
+
+    save_to = str(tmp_path.joinpath('test.png'))
+
+    red_pixel = Image.fromarray(np.array([[[255, 0, 0]]], dtype=np.uint8), 'RGB')
+    green_pixel = Image.fromarray(np.array([[[0, 255, 0]]], dtype=np.uint8), 'RGB')
+    blue_pixel = Image.fromarray(np.array([[[0, 0, 255]]], dtype=np.uint8), 'RGB')
+
+    proj = Project.new(1, 1)
+    proj.add_layer(red_pixel, 'red')
+    proj.add_layer(green_pixel, 'green')
+    proj.add_layer(blue_pixel, 'blue')
+
+    rend = Renderer(proj)
+    rend.render().save(save_to)
+    img = Image.open(save_to)
+
+    # by default, layers are stacked downward
+    assert list(np.asarray(img)[0][0]) == [255, 0, 0, 255]
+
+    # this can be overridden by using explicit z index
+    proj = Project.new(1, 1)
+    proj.add_layer(red_pixel, 'red', z_index=1)
+    proj.add_layer(green_pixel, 'green', z_index=2)
+    proj.add_layer(blue_pixel, 'blue', z_index=3)
+
+    rend = Renderer(proj)
+    rend.render().save(save_to)
+    img = Image.open(save_to)
+
+    assert list(np.asarray(img)[0][0]) == [0, 0, 255, 255]
 
